ec146792129ace81c1e598c8be3964df3bcdf17c
senthilravi12 <senthilravi12@gmail.com>
2022-05-09 08:27:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
+$headers=[
+        'User-Agent: Example REST API Client',
+        'Authorization :token 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 '
+    ];
         $ch=curl_init("https://github.com/senthilravi12/curl");
+
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+       
+
 
         curl_exec($ch);
 
